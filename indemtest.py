5645abd9871803ace12cc27e35d30cf7a47ddc20
agusfcastro <c.federico.agustin@gmail.com>
2022-02-20 18:46:05 -0300

    Proyecto calculadora de indemnizcaciones

---
+++ indemtest.py
@@ -0,0 +1,29 @@
+def almacen_meses():
+    ingreso_temp = {
+        'mes_1': str(input('ingreso: ')),
+        'mes_2': str(input('ingreso: ')),
+        'mes_3': str(input('ingreso: ')),
+        'mes_4': str(input('ingreso: ')),
+        'mes_5': str(input('ingreso: ')),
+        'mes_6': str(input('ingreso: ')),
+        'mes_7': str(input('ingreso: ')),
+        'mes_8': str(input('ingreso: ')),
+        'mes_9': str(input('ingreso: ')),
+        'mes_10': str(input('ingreso: ')),
+        'mes_11': str(input('ingreso: ')),
+        'mes_12': str(input('ingreso: ')),
+    }
+
+    temp = mes_1
+def run():
+    pass
+    
+print('''
+Hola! soy Pepe la calculadora de indemnizaciones de Agustín!
+antes de comenzar, debes ingresar unos datos para que pueda
+funcionar. No te preocupes! te dare las instrucciones necesarias.''')
+
+
+
+if __name__ == '__main__':
+    run()
